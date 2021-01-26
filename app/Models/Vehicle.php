9e844fe8489911dbd74198381986dcaf50711909
William <work@suppo.fr>
2021-01-26 15:46:15 +0100

    Création des modèles.

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle extends Model
+{
+    use HasFactory;
+
+    protected $table = 'vehicles';
+
+    protected $primaryKey = 'id';
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function customers()
+    {
+        return $this->belongsToMany(User::class)->withPivot('started_at', 'ended_at');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Models\Brand;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $users = User::all();
+
+    echo "<h1>Les utilisateurs</h1>";
+    echo "<ul>";
+
+    foreach ($users as $user) {
+        echo "<li>#$user->id $user->name (score: $user->score, porte-monnaie: $user->wallet, role: $user->role, actif: $user->enabled)</li>";
+    }
+
+    echo "</ul>";
+
+    $brands = Brand::all();
+
+    echo "<h1>Les marques</h1>";
+    echo "<ul>";
+    foreach ($brands as $brand) {
+        echo "<li>";
+        echo "#$brand->id $brand->name (premium: $brand->premium)";
+
+        echo "<ul>";
+        foreach ($brand->vehicles as $vehicle) {
+            echo "<li>#$vehicle->id $vehicle->name (type: $vehicle->type, km: $vehicle->odometer, statut: $vehicle->status)</li>";
+        }
+
+        echo "</ul>";
+        echo "</li>";
+    }
+    echo "</ul>";
+
+    $users = User::has('vehicles')->get();
+
+    echo "<h1>Les locations par client</h1>";
+    echo "<ul>";
+
+    foreach ($users as $user) {
+        echo "<li>";
+        echo "$user->name :";
+
+        echo "<ul>";
+        foreach ($user->vehicles as $vehicle) {
+            echo "<li>" . $vehicle->brand->name . " $vehicle->name (début: " . $vehicle->pivot->started_at . ", fin: " .$vehicle->pivot->ended_at . ")</li>";
+        }
+        echo "</ul>";
+        echo "</li>";
+    }
+    echo "</ul>";
 });
