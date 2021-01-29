ccbc3e90d752bf6d88936a682d2833a8d686180d
William <work@suppo.fr>
2021-01-29 09:02:27 +0100

    Fichier de test pour le VehicleService.

---
+++ tests/Unit/VehicleServiceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class VehicleServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_save_vehicle()
+    {
+        // Préparer des données
+            // Création d'une marque
+            // Les paramètres du véhicule
+        // Instancier un VehicleService
+        // Utiliser la méthode saveVehicle
+    }
+}
